07230c458c1c4b66b0e1a4516dd567be5673bd21
BrandonComstock <brandoncomstock40@gmail.com>
2025-05-01 20:51:02 -0500

    passed first test on exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(array) {
+    let newArray = [];
+    for (let i = 0; i < array.length; i++) {
+        for (let j = 1; j <= arguments.length - 1; j++) {
+            if (array[i] != arguments[j]) {
+                newArray.push(array[i]);
+            }
+        }
+    }
+    return newArray;
 };
 
 // Do not edit below this line
