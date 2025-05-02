ee37bed19a0ff2a7d37b25a3774587decdcf9e32
BrandonComstock <brandoncomstock40@gmail.com>
2025-05-02 09:51:17 -0500

    realized bug in code, need to refactor. pushes value into newarray for every argument instead of only once

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,10 +1,11 @@
 const removeFromArray = function(array) {
-    let newArray = [];
-    for (let i = 0; i < array.length; i++) {
-        for (let j = 1; j <= arguments.length - 1; j++) {
-            if (array[i] != arguments[j]) {
+const newArray = [];
+    for (let i = 0; i <= array.length - 1; i++) {
+        for (let j = 0; j < arguments.length - 1; j++) {
+            //pushes value to newArray for each arugment, only need to push it one time.
+            if (array[i] !== arguments[j + 1]) {
                 newArray.push(array[i]);
-            }
+            } 
         }
     }
     return newArray;
