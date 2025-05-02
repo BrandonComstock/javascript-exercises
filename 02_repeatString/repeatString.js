1c101f2d1177d23a95e46d89aef4a5447c9c173d
BrandonComstock <brandoncomstock40@gmail.com>
2025-05-01 19:50:32 -0500

    passed first 2 exercises

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+    let newString = "";
+    if (num >= 0) {
+        for (let i = 0; i < num; i++) {
+            newString += str;
+        }
+        return newString;
+    } else {
+        return "ERROR";
+    }
+   
 };
 
 // Do not edit below this line
