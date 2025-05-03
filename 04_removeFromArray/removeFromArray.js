bd9f75b1f1d81e5a747c154fbb65812ed4520669
BrandonComstock <brandoncomstock40@gmail.com>
2025-05-03 08:05:29 -0500

    refactored removeFromArray function, passing first test

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,10 +1,9 @@
 const removeFromArray = function(array) {
-const newArray = [];
+    const newArray = array.slice();
     for (let i = 0; i <= array.length - 1; i++) {
         for (let j = 0; j < arguments.length - 1; j++) {
-            //pushes value to newArray for each arugment, only need to push it one time.
-            if (array[i] !== arguments[j + 1]) {
-                newArray.push(array[i]);
+            if (array[i] === arguments[j + 1]) {
+                newArray.splice(i, 1);
             } 
         }
     }
