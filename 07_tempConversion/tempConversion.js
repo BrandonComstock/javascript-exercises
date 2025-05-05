3ef026aae325351bd319e3bf042c7190a0d2dbbb
BrandonComstock <brandoncomstock40@gmail.com>
2025-05-05 18:44:32 -0500

    passed all test for 07, convert to fahrenheit

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
 const convertToCelsius = function(num) {
-  num -= 32
+  num -= 32;
   return (Math.round((num * 5/9) * 10) / 10);
 };
 
 const convertToFahrenheit = function(num) {
+  num *= 9/5;
+  return (Math.round((num + 32) * 10) / 10);
 };
 
 // Do not edit below this line
