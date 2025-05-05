67e2ab709626d75ec36f0c258da7c0882ef9393f
BrandonComstock <brandoncomstock40@gmail.com>
2025-05-04 19:41:21 -0500

    05 works with negative numbers

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -3,19 +3,26 @@ let sum = 0;
 let smaller;
 let bigger;
 
-if (num1 < num2) {
-    smaller = num1;
-    bigger = num2;
+if (num1 < 0 || num2 < 0) {
+    sum = "ERROR"
 } else {
-    smaller = num2;
-    bigger = num1;
+    if (num1 < num2) {
+        smaller = num1;
+        bigger = num2;
+    } else {
+        smaller = num2;
+        bigger = num1;
+    }
+    
+        for (let i = smaller - 1; i < bigger; i++) {
+            sum += i + 1;
+        }   
+    
 }
 
-    for (let i = smaller - 1; i < bigger; i++) {
-        sum += i + 1;
-    }   
+return sum;
 
-    return sum;
+
 };
 
 // Do not edit below this line
