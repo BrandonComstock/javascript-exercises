1d721d87d7d04ca279f55461932baae05278b32a
BrandonComstock <brandoncomstock40@gmail.com>
2025-05-05 18:42:33 -0500

    passed all test on 07 for first function, convert to celsius

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,9 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(num) {
+  num -= 32
+  return (Math.round((num * 5/9) * 10) / 10);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(num) {
 };
 
 // Do not edit below this line
