d619544656056a4af2df91f3c33cea569d3e3c2a
BrandonComstock <brandoncomstock40@gmail.com>
2025-05-05 18:50:49 -0500

    08 calculator, made sum function. iterated over each element and added it to variable

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -6,8 +6,14 @@ const subtract = function(num1, num2) {
 	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  let sum = 0
+
+  array.forEach(element => {
+    sum += element;
+  });
+  
+	return sum;
 };
 
 const multiply = function() {
