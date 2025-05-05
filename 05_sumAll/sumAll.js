61c1086398f258594509a3808af1ba917e3d9ed2
BrandonComstock <brandoncomstock40@gmail.com>
2025-05-04 19:28:42 -0500

    passed first test on 05, first attempt at sumall function

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+let sum = 0;
+let smaller;
+let bigger;
 
+if (num1 < num2) {
+    smaller = num1;
+    bigger = num2;
+} else {
+    smaller = num2;
+    bigger = num1;
+}
+
+    for (let i = smaller - 1; i < bigger; i++) {
+        sum += i + 1;
+    }   
+
+    return sum;
 };
 
 // Do not edit below this line
