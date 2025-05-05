578d4d243ba884077ca3d1b34aaf58372ff46734
BrandonComstock <brandoncomstock40@gmail.com>
2025-05-04 19:14:06 -0500

    passed second test on exercise 04, added subtractor variable to change splice index

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,9 +1,11 @@
 const removeFromArray = function(array) {
     const newArray = array.slice();
+    let subtractor = 0;
     for (let i = 0; i <= array.length - 1; i++) {
         for (let j = 0; j < arguments.length - 1; j++) {
             if (array[i] === arguments[j + 1]) {
-                newArray.splice(i, 1);
+                newArray.splice(i + subtractor, 1);
+                subtractor--;
             } 
         }
     }
