fc9da0a5bda5dde3823e7aad3102f9f92e7109fb
BrandonComstock <brandoncomstock40@gmail.com>
2025-05-08 18:33:56 -0500

    palendromes first test passed, iterated oiver text made new text and compared

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
+const palindromes = function (text) {
+    text = text.replaceAll(" ","").toLowerCase();
+    let textBackwards = "";
+    for (let i = text.length - 1; i >= 0; i--) {
+        textBackwards += text[i];
+    }
 
+    return (text === textBackwards);
 };
 
 // Do not edit below this line
