4793458fa5c7a961184849c101c4f9da251e9c8d
BrandonComstock <brandoncomstock40@gmail.com>
2025-05-08 18:59:23 -0500

    10 fibonacci first test passed

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,9 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    let sequence = [1, 1];
+    for (let i = 1; i < num; i++) {
+        sequence.push((i-1) + (i-2));
+    }
+    return sequence[num];
 
 };
 
