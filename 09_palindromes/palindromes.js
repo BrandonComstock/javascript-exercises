d8f8b37fd9f73f14f439432173bccc93a2b12c9d
BrandonComstock <brandoncomstock40@gmail.com>
2025-05-08 18:48:15 -0500

    09 palindromes all test passed, text only works with letters and numbers

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,5 @@
 const palindromes = function (text) {
-    text = text.replaceAll(" ","").toLowerCase();
+    text = text.replace(/[^a-zA-Z0-9]/g,"").toLowerCase();
     let textBackwards = "";
     for (let i = text.length - 1; i >= 0; i--) {
         textBackwards += text[i];
