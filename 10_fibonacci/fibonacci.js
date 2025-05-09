b81fe71f66394574228858182013a8a28603f87e
BrandonComstock <brandoncomstock40@gmail.com>
2025-05-08 19:11:24 -0500

    10 fibonacci attempt at making first and second preceeding number equal 0 if its undefined or NaN

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,7 +1,15 @@
 const fibonacci = function(num) {
     let sequence = [1, 1];
-    for (let i = 1; i < num; i++) {
-        sequence.push((i-1) + (i-2));
+    for (let i = 0; i <= num; i++) {
+        let first = sequence[i - 1];
+        let second = sequence[i - 1];
+        if (typeof first !== "number") {
+            first = 0;
+        }
+        if (typeof second !== "number") {
+            second = 0;
+        }
+        sequence.push(first + second);
     }
     return sequence[num];
 
