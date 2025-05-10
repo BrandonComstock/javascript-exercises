28b5e7e12a201d01d962833ee280a35ff7cdca43
BrandonComstock <brandoncomstock40@gmail.com>
2025-05-09 19:56:38 -0500

    12 first attempt, no tests passing. used reduce function on people array

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    let person = {};
+    function reducer (personAccumulator, personCurrent, index)  {
+        let age1 = personAccumulator.yearOfDeath - personAccumulator.yearOfBirth;
+        let age2 = personCurrent.yearOfDeath - personCurrent.yearOfBirth;
+        
+        if (age1 > age2) {
+            return age1;
+        } else {
+            return age2;
+        }
+    };
 
+    return people.reduce(reducer);
 };
 
 // Do not edit below this line
