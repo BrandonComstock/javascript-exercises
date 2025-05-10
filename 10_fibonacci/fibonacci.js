e89503334e62fd11a161bcd500370d99e94f3ee4
BrandonComstock <brandoncomstock40@gmail.com>
2025-05-09 19:12:35 -0500

    added conditionals if num is 0 or negative, works with num as string now. all tests passing

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,9 @@
 const fibonacci = function(num) {
+    num = Number(num);
     let sequence = [1, 1];
+    if (num === 0) return 0;
+    if (num < 0) return "OOPS";
+
     for (let i = 2;  i < num; i++) {
         let first = sequence[i - 1];
         let second = sequence[i - 2];
