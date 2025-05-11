4e4d674b8f385c4b3a360eea796a0416c3a38ee2
BrandonComstock <brandoncomstock40@gmail.com>
2025-05-10 20:03:12 -0500

    12 passing all tests, checked if current person is still alive

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,8 +1,20 @@
 const findTheOldest = function(people) {
     let person = {};
-    function reducer (personAccumulator, personCurrent, index)  {
-        let age1 = personAccumulator.yearOfDeath - personAccumulator.yearOfBirth;
-        let age2 = personCurrent.yearOfDeath - personCurrent.yearOfBirth;
+    function reducer (personAccumulator, personCurrent)  {
+        const currentDate = new Date();
+        let age1 = 0;
+        let age2 = 0;
+        if  (personAccumulator.yearOfDeath && personCurrent.yearOfDeath) {
+            age1 = personAccumulator.yearOfDeath - personAccumulator.yearOfBirth;
+            age2 = personCurrent.yearOfDeath - personCurrent.yearOfBirth;
+        } else if (!personAccumulator.yearOfDeath) {
+            age1 = currentDate.getFullYear() - personAccumulator.yearOfBirth;
+            age2 = personCurrent.yearOfDeath - personCurrent.yearOfBirth;
+        } else if (!personCurrent.yearOfDeath) {
+            age2 = currentDate.getFullYear() - personAccumulator.yearOfBirth;
+            age1 = personAccumulator.yearOfDeath - personAccumulator.yearOfBirth;
+        }
+   
         
         if (age1 > age2) {
             return personAccumulator;
